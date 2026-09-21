ebb92a75f09290da3abe89a4776cd2ffc1a2b8e3
peter@pinchez <petermutua498@gmail.com>
2026-09-21 21:05:54 +0300

    commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
-
+const Payment = require("./models/Payment");
 // Routes
 const paymentRoutes = require("./routes/paymentRoutes");
 const packageRoutes = require("./routes/packageRoutes");
